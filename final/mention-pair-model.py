19b06d56a8a58ecc019b586bb87ef0d07c5dc583
jiahuic <jiahui.k.chen@gmail.com>
2019-11-16 11:17:58 -0700

    Mention pair model implemented. Lots more feature files..

--- final/mention-pair-model.py
+++ final/mention-pair-model.py
@@ -3,8 +3,11 @@
 # RUN BY: python mention-pair-model.py <feature-list> - where feature-list is a text file with a feature file on each line
 
 from sklearn.tree import DecisionTreeClassifier 
-import sys
+from sklearn.model_selection import cross_val_score
 import numpy as np
+
+import sys
+from joblib import dump, load
 
 
 # Loading in features 
@@ -27,9 +30,31 @@ with open(feature_list) as feature_file:
 
 pos_data = np.concatenate(pos_features, axis=0)
 neg_data = np.concatenate(neg_features, axis=0)
+all_data = np.concatenate((pos_data, neg_data), axis=0)
+all_labels = np.concatenate((np.ones((pos_data.shape[0])), np.zeroes(neg_data.shape[0])))
 
 ######################################Train model
+# Cross validate on tree depth and minimum samples needed to split a node
+depths = [2, 4, 6, 8]
+min_samples = [2, 4, 6, 8]
 
-#####################################Test Model
+best_acc = 0
+best_params = () # (best depth, best min sample split)
+for depth in depths: 
+    for min_sample in min_samples:
+        tree = DecisionTreeClassifier(max_depth=depth, min_samples_split=min_sample)
+        cv_scores = cross_val_score(tree, all_data, all_labels, cv=5) 
+        cv_mean = np.mean(cv_scores)
+        if cv_mean > best_acc:
+            best_acc = cv_mean
+            best_params[0] = depth
+            best_params[1] = min_sample
+
+print(f"BEST CROSS VALIDATION MEAN ACC: {best_acc}")
+
+# Train model on entire dataset with best parameters
+tree = DecisionTreeClassifier(max_depth=best_params[0], min_samples_split=best_params[1])
+tree.fit(all_data, all_labels)
 
 #####################################Save Model...
+dump(tree, "mention-pair-tree.joblib")
